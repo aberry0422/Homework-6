06afe189d49f9edcc1e0ea4086df5907589cf53e
Ashawn Berry <c.berry898@gmail.com>
2020-03-18 09:16:30 -0400

    Initial Commit

---
+++ Homework 6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 6
+//
+//  Created by Ashawn C Berry on 3/18/20.
+//  Copyright © 2020 Ashawn C Berry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
